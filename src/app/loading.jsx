317dfb2a614ac7c67e52c75ede7448d11390cd76
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2024-11-30 19:42:23 +0300

    :construction: Adding loading element with Next.js

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <div className={"flex flex-row justify-center align-middle"}>
+      <img src={"Infinity.svg"} alt="loading" className={"h-96"}/>
+      {/*  <div className={"animate-spin h-10 w-10 border-t-2 border-b-2 border-gray-900 rounded-full"}></div>*/}
+      {/*  <h2 className={"text-2xl text-gray-900"}>Loading...</h2>*/}
+    </div>
+  );
+}
+
+// #270071 color
